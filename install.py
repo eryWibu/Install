0213c582cc1b802a5d00bfefff74b289e7ae94fb
ERY <100246835+eryWibu@users.noreply.github.com>
2022-07-17 10:41:05 +0700

    Update install.py

--- install.py
+++ install.py
@@ -3,13 +3,9 @@ from rich import print as prints
 from rich.panel import Panel
 
 os.system("clear")
-print(" [!]  Subscribe Youtube Ery Devs")
-time.sleep(4)
-os.system("xdg-open https://youtube.com/channel/UCTpvwoW8MDOut6wln7pRZZQ")
-os.system("clear")
 prints(Panel(f"[deep_pink3] @Creator: Ery Israndi \n Youtube : Ery Devs \n Github  : eryWibu.com"))
 
-prints(Panel(f"[deep_pink3] [1]. Install Packages \n [2]. Link Termux  \n [3]. Exit"))
+prints(Panel(f"[deep_pink3] [1]. Install Packages \n [2]. Link Termux  \n [3]. Subscribe Youtube Ery Devs[4]. Exit"))
 
 wa = input("[?] Pilih : ")
 
@@ -22,10 +18,10 @@ if wa =="1":
    os.system("pip3 install requests bs4 colorama")
    os.system("pip2 install requests bs4 colorama")
    prints(Panel(f"[deep_pink3] [!] Install Selesai"))
-   time.sleep(5)
-   os.system("python install.py")
 if wa =="2":
    os.system("xdg-open https://www.mediafire.com/file/k4tya4lmxbxi7rb/com.termux_118.apk/file")
    os.system("python install.py")
-if wa =="3":
+if wa =="4":
    os.system("exit")
+if wa =="3":
+   os.system("https://youtube.com/channel/UCTpvwoW8MDOut6wln7pRZZQ")
